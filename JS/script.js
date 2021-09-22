145e30710484fb4c2a8b08053bee370b0ab1f062
Ashley <atallashnic@gmail.com>
2021-09-21 21:18:00 -0700

    take home challenge complete!

---
+++ JS/script.js
@@ -0,0 +1,10 @@
+// Single line comment
+/* Multi-line comment 
+here's that multi-line business in action
+ohh yeahh
+it's so good */
+const myName = "Ashley";
+const greeting = `Hi, my name is ${myName}`;
+console.log(greeting);
+const faveFood = "Sushi, or Sweets";
+console.log(faveFood);
