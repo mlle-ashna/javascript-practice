f036eac0a702122358c93a0fc4d650e1f18e98c2
Ashley <atallashnic@gmail.com>
2021-09-23 19:41:42 -0700

    Day Two Daily Challenge

--- JS/script.js
+++ JS/script.js
@@ -7,3 +7,13 @@ const myName = "Ashley";
 const greeting = `Hi, my name is ${myName}`;
 console.log(greeting);
 const faveFood = "Sushi, or Sweets";
+// DAY TWO DAILY CHALLENGE
+let bigInt = 66608978462424333507079666070787777223232333069808000162558777n;
+let sCfL = "Supercalifragilisticexpialidocious";
+console.log(sCfL.length);
+console.log(69);
+console.log(bigInt);
+console.log(typeof 69);
+console.log(typeof sCfL);
+console.log(typeof bigInt);
+// END DAY TWO DAILY CHALLENGE 
